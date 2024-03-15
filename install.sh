40e7a357da174335c55d58601470aff111a094cf
CakeSystem <1625000148@qq.com>
2024-03-15 23:36:08 +0800

    3.9.8

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="3.9.7"
+VERSION="3.9.8"
 DOWNLOAD_HOST="https://github.com/CakeSystem/CakeMinerProxy/raw/main/linux"
 ORIGIN_EXEC="CakeMinerProxy-${VERSION}"
 
@@ -375,6 +375,7 @@ ISSUE() {
     echo "3.9.4"
     echo "3.9.6"
     echo "3.9.7"
+    echo "3.9.8"
 }
 
 filterResult() {
