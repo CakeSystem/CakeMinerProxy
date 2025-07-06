86b0744d324440925c39073431bb37b008cc2f8b
CakeSystem <1625000148@qq.com>
2025-07-07 01:19:37 +0800

    4.5.0

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="4.4.9"
+VERSION="4.5.0"
 DOWNLOAD_HOST="https://github.com/CakeSystem/CakeMinerProxy/raw/main/linux"
 ORIGIN_EXEC="CakeMinerProxy-${VERSION}"
 
@@ -400,6 +400,7 @@ ISSUE() {
     echo "4.4.7"
     echo "4.4.8"
 	echo "4.4.9"
+    echo "4.5.0"
 }
 
 filterResult() {
